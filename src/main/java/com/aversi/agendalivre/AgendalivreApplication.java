20b0565bf977ceb90709bac1c942e33284c17c26
aversii <lucasaversi@gmail.com>
2025-03-11 12:05:51 -0300

    initial-commit: dependencies and basic springboot template

---
+++ src/main/java/com/aversi/agendalivre/AgendalivreApplication.java
@@ -0,0 +1,13 @@
+package com.aversi.agendalivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendalivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendalivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aversi/agendalivre/AgendalivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aversi.agendalivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendalivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
